236899b1319014eef4370885fbafc43bb12dea13
Qaziaaaa <qazithekingston@gmail.com>
2026-07-28 23:27:40 +0500

    Add Dashboard page with poll feed, skeleton loading, and mobile bottom nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Layout from "./components/Layout.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
 function App() {
     return (
@@ -15,7 +16,7 @@ function App() {
                     <Route path="/forgot-password" element={<ForgotPasswordPage />} />
                     <Route path="/" element={<Navigate to="/dashboard" replace />} />
                     <Route element={<Layout />}>
-                        {/* Pages wrapped in Layout will go here */}
+                        <Route path="/dashboard" element={<Dashboard />} />
                     </Route>
                     <Route path="*" element={<Navigate to="/login" replace />} />
                 </Routes>
